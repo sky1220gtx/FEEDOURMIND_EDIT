75999cae745fd1b11bbdf1abe130eda80e9223a3
henryust12 <henryust12@gmail.com>
2021-07-22 08:50:28 +0900

    edited tbl_user

--- db/db_fom.sql
+++ db/db_fom.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 21, 2021 at 11:18 PM
--- Server version: 10.3.28-MariaDB
--- PHP Version: 7.2.24
+-- Generation Time: Jul 22, 2021 at 01:47 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,15 +30,12 @@ USE `db_fom`;
 --
 
 DROP TABLE IF EXISTS `tbl_comment`;
-CREATE TABLE IF NOT EXISTS `tbl_comment` (
-  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_comment` (
+  `comment_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `comment` text NOT NULL,
   `created_date` date NOT NULL,
-  `post_id` int(11) NOT NULL,
-  PRIMARY KEY (`comment_id`),
-  KEY `post_id` (`post_id`),
-  KEY `tbl_comment_ibfk_1` (`user_id`)
+  `post_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -48,15 +45,14 @@ CREATE TABLE IF NOT EXISTS `tbl_comment` (
 --
 
 DROP TABLE IF EXISTS `tbl_contact`;
-CREATE TABLE IF NOT EXISTS `tbl_contact` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_contact` (
+  `id` int(11) NOT NULL,
   `Fname` varchar(20) NOT NULL,
   `Lname` varchar(20) NOT NULL,
   `Email` varchar(50) NOT NULL,
   `Message` text NOT NULL,
-  `INSERT_NB` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `INSERT_NB` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -65,13 +61,11 @@ CREATE TABLE IF NOT EXISTS `tbl_contact` (
 --
 
 DROP TABLE IF EXISTS `tbl_post`;
-CREATE TABLE IF NOT EXISTS `tbl_post` (
-  `post_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_post` (
+  `post_id` int(11) NOT NULL,
   `created_date` date NOT NULL,
   `post` text NOT NULL,
-  `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`post_id`),
-  KEY `user_id` (`user_id`)
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -81,22 +75,85 @@ CREATE TABLE IF NOT EXISTS `tbl_post` (
 --
 
 DROP TABLE IF EXISTS `tbl_user`;
-CREATE TABLE IF NOT EXISTS `tbl_user` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_user` (
+  `user_id` int(11) NOT NULL,
   `user_name` varchar(10) NOT NULL,
   `name` varchar(200) NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `password` varchar(250) NOT NULL COMMENT 'password hash --> bcrypt',
   `email` varchar(200) NOT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8;
+  `email_status` char(1) DEFAULT '0',
+  `slack_user_id` varchar(200) DEFAULT NULL,
+  `slack_user_name` varchar(200) DEFAULT NULL,
+  `CREATE` datetime NOT NULL DEFAULT current_timestamp(),
+  `UPDATE` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tbl_user`
 --
 
-INSERT INTO `tbl_user` (`user_id`, `user_name`, `name`, `password`, `email`) VALUES
-(1, 'shima', 'Henry Simarmata', 'abc', 'henryust12@gmail.com'),
-(112, 'evren', 'evren balik', 'evren123', 'evren@gmail.com');
+INSERT INTO `tbl_user` (`user_id`, `user_name`, `name`, `password`, `email`, `email_status`, `slack_user_id`, `slack_user_name`, `CREATE`, `UPDATE`) VALUES
+(1, 'shima', 'Henry Simarmata', '$2y$10$OLjFkL.wma8DG32IL5BGru2QpRC6DlSmXTtSTe2wlBrUS8HtB0Opm', 'henryust12@gmail.com', '1', NULL, NULL, '2021-07-22 08:36:06', '2021-07-22 08:36:06'),
+(112, 'evren', 'evren balik', '$2y$10$yotpNU3JRFncq.je3J.YteAGTwtA9c4ygCuDraaUamFZY7d94CBVa', 'evren@gmail.com', '1', NULL, NULL, '2021-07-22 08:36:06', '2021-07-22 08:36:06');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_comment`
+--
+ALTER TABLE `tbl_comment`
+  ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `post_id` (`post_id`),
+  ADD KEY `tbl_comment_ibfk_1` (`user_id`);
+
+--
+-- Indexes for table `tbl_contact`
+--
+ALTER TABLE `tbl_contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_post`
+--
+ALTER TABLE `tbl_post`
+  ADD PRIMARY KEY (`post_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_comment`
+--
+ALTER TABLE `tbl_comment`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_contact`
+--
+ALTER TABLE `tbl_contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_post`
+--
+ALTER TABLE `tbl_post`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10000;
 
 --
 -- Constraints for dumped tables
