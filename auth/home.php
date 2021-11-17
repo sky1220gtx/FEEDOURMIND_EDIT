0d39964d2c0e6fc2181d77a379637fed4183ec98
henryust12 <henryust12@gmail.com>
2021-11-17 17:20:58 +0900

    added sample article view page

--- auth/home.php
+++ auth/home.php
@@ -5,6 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FOM</title>
+    <style>
+        .uid{
+            font-size: x-small;
+        }
+    </style>
 </head>
 <body>
     <?php
@@ -19,6 +24,29 @@
         echo '<a href="login.php">HERE</a>';
         echo '<hr> OR Register <br>';
         echo '<a href="register.php">HERE</a>';
+    }
+
+    echo '<hr>';
+
+
+    // -------
+    include_once 'config.php';
+    // SELECT  START
+    $query = 'SELECT * FROM `fom_tblArticle`';
+    $stmt = $dbh->prepare($query);
+    
+    $stmt->execute();
+
+    if($stmt){
+        foreach ($stmt as $row) {
+            echo '<br>';
+            // Session Start
+            echo '<h1>'.$row['title'].'<span class="uid"> by '.$row['userID'].'</span></h1>';
+            echo '<p>'.$row['content'].'</p>';
+            echo '<p>'.$row['VideoLink'].'</p>';
+            echo '<hr>';
+            // Session End
+        }
     }
     ?>
     
