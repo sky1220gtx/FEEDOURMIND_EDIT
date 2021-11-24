e6b51458404d8859646089d5029ab2adee5a1013
henryust12 <henryust12@gmail.com>
2021-11-24 15:47:18 +0900

    added php Codes

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+// Variables
+$fom_title = 'Feed Our Mind';
+?>
+<?php
+// Headerの入れる場所だけ
+include 'template/all_header.php';
+?>
+<link rel="stylesheet" href="assets/css/loginstyles.css">
+<?php
+// Body/Contentの入れる場所だけ
+include 'template/body/content_login.php'
+?>
+
+<?php
+// Footerの入れる場所だけ
+include 'template/footer/footer.php';
+?>
+<?php
+// javascriptの入れる場所だけ
+include 'template/javascript/js.php';
+?>
+</body>
+</html>
---
+++ template/all_header.php
@@ -0,0 +1,8 @@
+<?php
+include 'header/body_header.php';
+
+include 'header/navbar.php';
+
+include 'header/side_navbar.php';
+
+?>
