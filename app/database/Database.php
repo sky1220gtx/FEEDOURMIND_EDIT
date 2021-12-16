338556c34bd63585ec2f9d02b6d12295bd40d789
henryust12 <henryust12@gmail.com>
2021-12-16 13:46:22 +0900

    change database config from local to internet

--- app/database/Database.php
+++ app/database/Database.php
@@ -1,8 +1,13 @@
 <?php
 
 class DB_FOM{
-    public static $hostname = 'localhost';
-    public static $dbname = 'web_app';
-    public static $username = 'master_user';
-    public static $password = 'master_user';
+    // public static $hostname = 'localhost';
+    // public static $dbname = 'web_app';
+    // public static $username = 'master_user';
+    // public static $password = 'master_user';
+
+    public static $hostname = '31.220.110.51';
+    public static $dbname = 'u730565153_fom';
+    public static $username = 'u730565153_Feedourmind21';
+    public static $password = 'Feedourmind21';
 }
