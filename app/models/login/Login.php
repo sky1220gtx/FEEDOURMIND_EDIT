7c55e90703c84a5f724adafc5ce2a269a7b83c9d
henryust12 <henryust12@gmail.com>
2021-12-20 06:12:51 +0900

    added sanitize code

--- app/models/login/Login.php
+++ app/models/login/Login.php
@@ -3,8 +3,8 @@
 
 class mLogin {
     public static function check_login(){
-        $u=$_POST['username'];
-        $p=$_POST['password'];
+        $u=htmlspecialchars($_POST['username']);
+        $p=htmlspecialchars($_POST['password']);
         $q = 'SELECT * FROM `fom_tblusers` WHERE userID="'.$u.'" AND password="'.$p.'"';
         return DB_FOM::FOM_SELECT($q);
     }
--- app/models/signup/Signup.php
+++ app/models/signup/Signup.php
@@ -1,17 +1,10 @@
 <?php
 
 class mSignup{
-    public static function check_login(){
-        $u=$_POST['username'];
-        $p=$_POST['password'];
-        $q = 'SELECT * FROM `fom_tblusers` WHERE userID="'.$u.'" AND password="'.$p.'"';
-        return DB_FOM::FOM_SELECT($q);
-    }
-
     public static function insert(){
-        $u=$_POST['username'];
-        $p=$_POST['password'];
-        $e=$_POST['email'];
+        $u=htmlspecialchars($_POST['username']);
+        $p=htmlspecialchars($_POST['password']);
+        $e=filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
         $q = 'INSERT INTO `fom_tblusers` (`userID`, `password`, `Email`, `Created`, `Updated`) VALUES ("'.$u.'", "'.$p.'", "'.$e.'", current_timestamp(), current_timestamp()); ';
         DB_FOM::FOM_INSERT($q);
         $q = 'SELECT * FROM `fom_tblusers` WHERE userID="'.$u.'" AND password="'.$p.'"';
