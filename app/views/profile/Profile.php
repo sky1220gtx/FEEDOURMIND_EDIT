cdf74a41a1d5c13c49a989700ffb37c89bd7bf2f
henryust12 <henryust12@gmail.com>
2021-12-20 05:56:37 +0900

    added more profile information

--- app/views/profile/Profile.php
+++ app/views/profile/Profile.php
@@ -10,12 +10,49 @@ include vPath.'templates/header.php';
         <?php
         if($_SESSION['fom_user_name'] != ''){
             echo '<P>“Welcome” '.$_SESSION['fom_user_name'].'</P>';
+            global $dataUser;
+            $_POST['username']=$_SESSION['fom_user_name'];
+            $_POST['password']=$_SESSION['fom_user_password'];
+            $dataUser = mLogin::check_login();
         } else {
             echo '<P>“Please Login First” </P>';
         }
         ?>
     </div>
-    </div>
+</div>
+<hr>
+
+<br>
+<table style="width:50%;">
+    <tr>
+        <td>User ID</td>
+        <td><?= $dataUser[0]['userID'] ?></td>
+    </tr>
+    <tr>
+        <td>First Name</td>
+        <td><?= $dataUser[0]['FirstName'] ?></td>
+    </tr>
+    <tr>
+        <td>Last Name</td>
+        <td><?= $dataUser[0]['LastName'] ?></td>
+    </tr>
+    <tr>
+        <td>Gender</td>
+        <td><?= $dataUser[0]['Gender'] ?></td>
+    </tr>
+    <tr>
+        <td>Birthday</td>
+        <td><?= $dataUser[0]['Birthday'] ?></td>
+    </tr>
+    <tr>
+        <td>Email</td>
+        <td><?= $dataUser[0]['Email'] ?></td>
+    </tr>
+    <tr>
+        <td>Website</td>
+        <td><?= $dataUser[0]['Site'] ?></td>
+    </tr>
+</table>
 <br><br>
 Clickl <a href="<?= rPath.'logout' ?>">Here</a> to logout
 
